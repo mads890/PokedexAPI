1bee7e842c05f74661f9be7315400db8715a785d
Madeline Guman <mads890@yahoo.com>
2020-06-17 22:45:06 -0400

    updated code for production

--- server.js
+++ server.js
@@ -1,15 +1,60 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
+const helmet = require('helmet')
+const POKEDEX = require('./pokedex.json')
 
 const app = express();
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting));
+app.use(helmet())
+app.use(cors()
 
-app.use(morgan('dev'));
-
-app.use((req, res) => {
-    res.send('response');
-});
-
-const PORT = 8000
-app.listen(PORT, () => {
-    console.log(`server listening at http://localhost:${PORT}`)
+app.use(function validateBearerToken(req, res, next) {
+    const authToken = req.get('Authorization')
+    const apiToken = process.env.API_TOKEN
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'unauthorized request' })
+    }
+    next()
 })
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+function handleGetTypes(req, res) {
+    res.json(validTypes)
+}
+
+app.get('/types', handleGetTypes)
+
+function handleGetPokemon(req, res) {
+    let response = POKEDEX.pokemon
+    const { name, type } = req.query;
+    if(name) {
+        response = response.filter(pokemon => {
+            pokemon.name.toLowerCase().includes(name.toLowerCase())
+        })
+    }
+    if(type) {
+        response = response.filter(pokemon => {
+            pokemon.type.includes(type)
+        })
+    }
+    res.status(200).json(response)
+}
+
+app.get('/pokemon', handleGetPokemon)
+
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT)
